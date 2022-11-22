7f1f52f16889be124c42ad8fdea4214bb0356424
Jae-Won Chung <jwnchung@umich.edu>
2022-11-22 00:40:24 -0500

    My version of the lualine theme

--- lua/lualine/themes/tundra.lua
+++ lua/lualine/themes/tundra.lua
@@ -5,28 +5,28 @@ local cp = require('nvim-tundra.palette.' .. vim.g.tundra_biome)
 
 M.normal = {
   a = { fg = cp.gray._400, bg = cp.gray._800, gui = 'bold' },
-  b = { fg = cp.gray._500, bg = cp.transparent, gui = 'bold' },
-  c = { fg = cp.gray._300, bg = cp.transparent },
+  b = { fg = cp.gray._500, bg = cp.gray._800, gui = 'bold' },
+  c = { fg = cp.gray._300, bg = cp.gray._800 },
 }
 
 M.insert = {
   a = { fg = cp.gray._900, bg = cp.green._600, gui = 'bold' },
-  b = { fg = cp.green._600, bg = cp.transparent, gui = 'bold' },
+  b = { fg = cp.green._600, bg = cp.gray._800, gui = 'bold' },
 }
 
 M.command = {
   a = { fg = cp.gray._900, bg = cp.opal._500, gui = 'bold' },
-  b = { fg = cp.opal._500, bg = cp.transparent, gui = 'bold' },
+  b = { fg = cp.opal._500, bg = cp.gray._800, gui = 'bold' },
 }
 
 M.visual = {
   a = { fg = cp.gray._900, bg = cp.orange._500, gui = 'bold' },
-  b = { fg = cp.orange._500, bg = cp.transparent, gui = 'bold' },
+  b = { fg = cp.orange._500, bg = cp.gray._800, gui = 'bold' },
 }
 
 M.replace = {
   a = { fg = cp.gray._900, bg = cp.sand._500, gui = 'bold' },
-  b = { fg = cp.sand._500, bg = cp.transparent, gui = 'bold' },
+  b = { fg = cp.sand._500, bg = cp.gray._800, gui = 'bold' },
 }
 
 return M
